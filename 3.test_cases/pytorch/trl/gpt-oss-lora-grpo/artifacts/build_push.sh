b42041a63bcffdb8e701149eba3f28d1120390c6
Mijanur Palash <p.mijanur@gmail.com>
2026-02-25 10:53:10 -0500

    Add GPT-OSS 20B LoRA + GRPO finetuning recipe

---
+++ 3.test_cases/pytorch/trl/gpt-oss-lora-grpo/artifacts/build_push.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+#set -e  # Exit on any error
+aws ecr-public get-login-password --region us-east-1 | docker login --username AWS --password-stdin public.ecr.aws
+echo "Building image ${REGISTRY}${IMAGE}:${TAG}"
+if docker buildx build --platform linux/amd64 -t ${REGISTRY}${IMAGE}:${TAG} .; then 
+    echo "Done building image!"
+    echo ""
+else
+    echo "Build failed!"
+    exit 1
+fi
+
+echo "Pushing image to ECR..."
+# Create registry if needed
+REGISTRY_COUNT=$(aws ecr describe-repositories | grep "${IMAGE}" | wc -l)
+if [ "$REGISTRY_COUNT" == "0" ]; then
+        aws ecr create-repository --repository-name ${IMAGE}
+fi
+
+# Login to registry
+echo "Logging in to $REGISTRY ..."
+if aws ecr get-login-password | docker login --username AWS --password-stdin $REGISTRY; then
+    echo "Login successful"
+else
+    echo "Login failed!"
+    exit 1
+fi
+
+# Push image to registry
+echo "Pushing image ${REGISTRY}${IMAGE}:${TAG}"
+if docker image push ${REGISTRY}${IMAGE}:${TAG}; then
+    echo "Done pushing image!"
+    echo ""
+else
+    echo "Push failed!"
+    exit 1
+fi
